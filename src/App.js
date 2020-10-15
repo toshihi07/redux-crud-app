6e017a281993de69df24cd4592a262a64d18c8b3
Toshihiro Shinmachi <toshihi0717@gmail.com>
2020-10-15 16:08:23 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React,{ Component } from 'react';
 
 class App extends Component{
   render() {
-    return <div><h1>Hello, world</h1></div>  
+    return React.createElement(
+      "h1",
+      null,
+      "Hellow World!!"
+    )
   }
 }
 
