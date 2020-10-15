67d2bc3416d339e0d9664ecd355a19abd1cae045
Toshihiro Shinmachi <toshihi0717@gmail.com>
2020-10-15 17:21:02 +0900

    props、map、defaultProps

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import React from "react";
 
-
-//クラスコンポーネント
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input
-//           type="text"
-//           onClick={() => {
-//             console.log("a");
-//           }}
-//         />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-
-const App = () =>{
+const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+    },
+    {
+      name: "Hanako",
+    },
+    {
+      name: "Kenta",
+      age: 15,
+    },
+  ];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Dog />
-      <Dog />
-    </div>  )
-}
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })}
+    </div>
+  );
+};
 
-const Dog = () =>{
-  return <div>わんわん</div>
-}
+const User = (props) => {
+  return (
+    <div>
+      Hi,Iam {props.name}! and {props.age} years old
+    </div>
+  );
+};
 
-const Cat = () =>{
-  return <div>にゃー</div>
+User.defaultProps = {
+  age:1
 }
 
 export default App;
